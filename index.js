2dede87671f772617b0af969d69a32a33f548db9
Lucky Ligovi <luckyligovi@gmail.com>
2022-08-29 01:26:14 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+
+// const button = document.getElementById("button");
+
+// function addingEventListener() {
+// 	button.addEventListener("click", function () {
+// 		alert("Hello World");
+// 	});
+// }
+
+// addingEventListener();
+
 function addingEventListener() {
+
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
 }
